5c349b479444846101d6d7d1d9081b3f6e9834e3
Nikolay <mykolagalanix@gmail.com>
2023-04-06 23:35:32 +0300

    add run.sh script

---
+++ src/Controller/MainController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MainController extends AbstractController
+{
+    #[Route('/qq')]
+    public function number(): Response
+    {
+        return new Response('hello');
+    }
+}
